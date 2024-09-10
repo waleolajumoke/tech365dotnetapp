302234c1e65491c127a337c99d4f32cfd4f91356
waleolajumoke <waleolajumoke@gmail.com>
2024-09-10 16:21:10 +0100

    pushing the mvc app

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace tech365dotnetapp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
